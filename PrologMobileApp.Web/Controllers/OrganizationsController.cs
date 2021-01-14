5c0be1ced5138cc9ccc32c4887d7c1d0497f83ae
Rhett <rhett.newton@gmail.com>
2021-01-13 21:33:05 -0600

    Rename file and new endpoint

---
+++ PrologMobileApp.Web/Controllers/OrganizationsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PrologMobileApp.Web.Models.DataTransfer;
+using PrologMobileApp.Web.Services;
+
+namespace PrologMobileApp.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrganizationsController : ControllerBase
+    {
+        private readonly ILogger<OrganizationsController> _logger;
+        private readonly IOrganizationService _organizationService;
+
+        public OrganizationsController(ILogger<OrganizationsController> logger, IOrganizationService organizationService)
+        {
+            _logger = logger;
+            _organizationService = organizationService;
+        }
+
+        [HttpGet]
+        [Route("summaries")]
+        public async Task<IEnumerable<OrganizationSummary>> Get()
+        {
+            return await _organizationService.GetOrganizationsSummaries();
+        }
+    }
+}
